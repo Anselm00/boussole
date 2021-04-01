595c2ac9de78679751307e98a288d8a998a3c30f
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-01 20:34:28 +0200

    no message

--- modal/tranquille.php
+++ modal/tranquille.php
@@ -3,12 +3,12 @@
 			<div class="modal-content">
 			  <h4>Tranquille</h4>
 			  
-			  <p>Soyons “soufis” ; des exaltés tranquilles !</p>
+			  <p>« Soyons “soufis” ; des exaltés tranquilles !</p>
 <p>Nous, Réalistes, en faisant notre devoir, nous n’avons décidé que de bien remplir notre vie ; sans attendre d’autre récompense parce que rien de mieux ne peut nous être offert.</p>
 <p>Que peut bien vouloir dire “bien remplir sa vie”, sinon la concevoir comme indissociable du bien du peuple, de l’humanité et du monde ?</p>
 <p>L’Histoire est notre justification absolue, et le Peuple son agent invincible ; c’est tout ce qu’il faut pour être exaltés-tranquilles.</p>
 <p>Nous ne sommes rien, mais la Caste Noire, même sans Église Réaliste, n’a jamais dormi que d’un oeil depuis 150 ans, tant la Masse l’épouvante ; que sera-ce avec un Front, et puis un grand Peuple ?</p>
-<p>Nous, ce “rien”, y aurons été pour quelque chose !</p></p>
+<p>Nous, ce “rien”, y aurons été pour quelque chose ! »</p></p>
 <p>__________________________</p>
 
 				  
---
+++ modal/travail-fecondite.php
@@ -0,0 +1,23 @@
+<!-- Modal Structure -->
+<div id="travail-fecondite" class="modal">
+			<div class="modal-content">
+			  <h4>Travail/Fécondité</h4>
+			  
+			  
+				<img class="image" src="img/travail-fecondite.png"/>
+				<p>__________________________</p>
+
+				<p>Voir<a href="https://www.eglise-realiste.org/pdf/Menage_Prive_ed.pdf" target="_blank"> "Ménage Privé"</a>, p.29 - Freddy Malot- mars 1999 - Editions de l'Evidence - 2010</p>
+				
+				<p>__________________________</p>
+
+				<img class="image" src="img/meneur.jpg"/>
+				<p>__________________________</p>
+
+				 <p>Voir<a href="https://www.eglise-realiste.org/pdf/Tableaux_ed.pdf" target="_blank"> "Tableaux"</a>, p.154 - Talib Freddy - mars 2006</p>
+			
+			</div>
+			<div class="modal-footer">
+			  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Vu</a>
+			</div>
+		  </div>
