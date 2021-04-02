880229b1039550c92dedb4b67d01e3cd9d9a467f
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-02 16:11:56 +0200

    no message

--- modal/ethnies.php
+++ modal/ethnies.php
@@ -3,6 +3,18 @@
 			<div class="modal-content">
 			  <h4>Ethnies</h4>
 			  
+
+			  </p>« L’ethnie travaille comme un corps social, avec un cerveau collectif, face au reste de la Race humaine. En réalité, dans le monde Parental, chaque ethnie fonctionne fondamentalement d’une manière analogue, selon la Matière et selon la Vie. C’est pourquoi chaque ethnie ne traite pas les étrangers comme des animaux ou des Génies (bienfaisants/malfaisants), mais comme d’autres hommes ; mais tous en bloc sont vus par chaque ethnie comme “moins hommes”. Ce sont “les tribus d’ailleurs” (OUMMOT HA-OLAM) ; les Noachides chez les juifs. Chaque ethnie se veut “l’ethnie-bijou” (peuple élu : AM-SEGOULLAH), seulement par suite de l’inégalité de développement des sociétés parentales. Car le monde matérialiste a une histoire ! L’élection ethnique se justifie en disant que la vraie <b>LOI DE VIE</b> est détenue par la parenté concernée. Insistons sur le fait que ladite Loi de Vie est le contraire de la Foi qui régit la société Politique. Cette Loi de Vie tient en deux mots : <b>FIDÉLITÉ</b> (ÈMOUNAH) au Pacte de Sang noué avec la Puissance Matérielle, côté objectif de la Loi ; et <b>CONFIANCE</b> (BITTAHON) dans la Promesse de domination terrestre qui est la récompense de la Fidélité, et est le côté subjectif de la Loi (ou Code de Vie).</p>
+</p> Comment se présente le Travail communautaire, conforme à la Fidélité, cette <b>VOIE coutumière</b> correcte (HALAKHAH), respectant les Commandements ? C’est l’annexion et les usages des DONS de la Nature, qui ne doit être ni <b>Imprudente</b>, ni <b>Ingrate</b> vis-à-vis de Mère Émanatrice, à qui la communauté doit son Sang et son Souffle (souffle élu parce que susceptible de paroles porteuses de magie efficace). Aux yeux des civilisés, la société Parentale est “écrasée par les forces de la nature”, superstitieuse, indolente et routinière. Chacun voit midi à son clocher ! L’aveuglement et l’injustice ne sont pas des arguments, et sont punis un jour ou l’autre. Que veut dire superstition ? C’est “vestige d’autrefois”. Jolie science ! Cela ne prouve qu’une chose : que les civilisés sont d’indécrottables cancres préhistoriques dans leur jugement de la société parentale, à qui ils doivent tout, et qui fut incomparablement plus stable que la société politique. Il était vraiment temps que le Réalisme Vrai mette les choses au point concernant cette affaire. Il en allait d’une compréhension enfin correcte du passé de l’humanité, et donc de son avenir. »</p>
+
+<p>__________________________</p>
+
+				  
+
+				  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Volontariat_Gratuite_ed.pdf" target="_blank"> "Volontariat-Gratuité"</a>, p.7-8 - août 2005 - Talib Freddy</p>
+
+				  <p>__________________________</p>
+
 			  <p>« Les Despotes Asiates ne se doutaient pas qu’avec eux la société Parentale avait 
 				  amassé tout ce qu’il fallait pour donner le jour à la société Politique, en particulier 
 				  avec leur pré-Écriture et leur pré-Monnaie. Et pour cause ! Pour ce faire, il fallait 
@@ -29,7 +41,7 @@
 
 				  
 
-				  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Esclavage_Egalit_Despot_ed.pdf" target="_blank"> "Despotisme Asiatique"</a>, p.14 - août 2005 - Talib Freddy - Editions de l'Evidence</p>
+				  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Esclavage_Egalit_Despot_ed.pdf" target="_blank"> "Despotisme Asiatique"</a>, p.14 - août 2005 - Talib Freddy - Editions de l'Evidence - 2010</p>
 			  
 
 			</div>
