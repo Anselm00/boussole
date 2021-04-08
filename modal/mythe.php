5213721187f54e82d1fb3e5bea1bb0fa79cd4065
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-08 20:55:57 +0200

    Nouveau titre

--- modal/mythe.php
+++ modal/mythe.php
@@ -3,6 +3,16 @@
 			<div class="modal-content">
 			  <h4>Mythe</h4>
 			  
+			  <p>« La clef du caractère anormal de l’importance donnée au judaïsme, et que traduit l’expression de civilisation “judéo-chrétienne”, n’est pas difficile à découvrir.</p>
+<p>Il n’est pas scandaleux, mais au contraire inévitable et fascinant, que le spiritualisme civilisé se donne un “Ancien Testament” ayant pour origine le Matérialisme primitif. La Religion doit vouloir posséder un fond qui échappe à l’Histoire, revendiquer et “nier” en même temps son passé Mythique.</p>
+<p>L’Hellénisme Jupitérien nous donne une expression simple du Mythe matérialiste adopté comme soubassement du Dogme spiritualiste, quand Pisistrate (- 560) collecte, transcrit et ordonne l’Iliade d’HOMÈRE, pour faire de cette épopée rapsodique la Préface de la Théogonie d’Hésiode. Ainsi, le propre Mythe des grecs sera fondateur de la religion de Zeus. »</p>
+
+<p>__________________________</p>
+
+<p>Voir <a href="https://www.eglise-realiste.org/pdf/Hebreux_ed.pdf" target="_blank"> "Les Hébreux, Peuple de l'échec?"</a>, p.43 - Talib Freddy - Eglise Réaliste Mondiale - octobre 2003 -  Editions de l'Evidence - 2010</p>
+			  
+<p>__________________________</p>
+
 			  <p>« Dans la Bible – le Code juif –, les questions de 
 			  Fécondité et de Matriarcat dominent tout le Mythe 
 			  matérialiste. À cela s’associe un conflit permanent entre 
@@ -16,7 +26,7 @@
 			  
 <p>__________________________</p>
 
-<p>« Dans la Genèse, il ne saurait s’agir d’un “Commencement” du Temps, tel que le conçoivent nécessairement des civilisés, c’est-à-dire en opposition et en relation à l’Éternité. La Genèse récite un Mythe, sans aucun souci de notre Temps, Mythe perdu au sein d’une durée apparentée au “Temps sans Bornes” du Zend Avesta. Par ce chant, le vieil Israël se transmet à lui-même, à la manière Coutumière tribale, un Conte vénérable qui justifie l’état vécu de la Permanence Répétitive.</p>
+<p>« Dans la Genèse, il ne saurait s’agir d’un “Commencement” du Temps, tel que le conçoivent nécessairement des civilisés, c’est-à-dire en opposition et en relation à l’Éternité. La Genèse récite un Mythe, sans aucun souci de notre Temps, Mythe perdu au sein d’une durée apparentée au “Temps sans Bornes” du Zend Avesta. Par ce chant, le vieil Israël se transmet à lui-même, à la manière Coutumière tribale, un Conte vénérable qui justifie l’état vécu de la Permanence Répétitive. »</p>
 <p>Le plus ridicule, dans cette histoire de “commencement” frauduleux de nos Bibles, c’est que les sionistes d’aujourd’hui fixent pratiquement le jour et l’heure de la création du monde ! Ainsi, nous serions aujourd’hui en l’an 5760… »</p>
 
 <p>__________________________</p>
--- modal/personnalisme.php
+++ modal/personnalisme.php
@@ -3,13 +3,7 @@
 			<div class="modal-content">
 			  <h4>Personnalisme</h4>
 			  
-			  <p>« Une seule chose compte : le Peuple, qu’il ne faut pas confondre avec l’Humanité. Si on sait ce que parler veut dire, nous sommes encore loin de pouvoir parler d’Humanité. Le mot n’aura un sens que dans un monde sans Argent et sans Armes. C’est pourquoi nous ne pouvons parler aujourd’hui que du Peuple. Le Peuple est ce qui peut et qui doit, et lui seul, tourner résolument et fermer le lourd volant de fonte de l’histoire dans le sens de l’avènement de l’Humanité.</p>
-<p>Pourquoi aller vers la création de l’Humanité, vers l’établissement de la Famille humaine délivrée du fléau de l’Argent et des Armes, passe-t-il pour une “utopie”, comme disent “les chefs du Système existant, dont la prétendue sagesse sera anéantie” (Saint Paul – I Cor. 2 : 6). Parce que la tâche de l’heure est de nous former en Peuple. En effet, s’il n’est pas d’Humanité, qui est notre but “final”, nous n’avons pas non plus de Peuple ; mais cela est notre but immédiat, nécessaire et possible, réellement à portée de notre main. »</p>
-
-<p>__________________________</p>
-
-<p>Voir <a href="https://www.eglise-realiste.org/pdf/KIKON-NE_ed.pdf" target="_blank"> "KIKON-NÊ"</a>, p.4 - Freddy Malot  - octobre 1999 -  Eglise Réaliste - Marxistes Amis de Dieu - Editions de l'Evidence - 2010</p>
-<p>__________________________</p>
+			  
 
 			  <p>« La Société Convenable voit les 2 sexes s’évader de la Cellule étroite du Ménage Privé (à présent détruite). On ne
 verse pas pour autant dans le concubinage légal actuel, qui réalise le voeu du “Cercle des Affranchis”
