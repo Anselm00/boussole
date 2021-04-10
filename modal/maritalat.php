7b38ab0cbab41627a10370efb9f34e361adecb3e
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-10 21:28:55 +0200

    no message

--- modal/maritalat.php
+++ modal/maritalat.php
@@ -3,10 +3,17 @@
 			<div class="modal-content">
 			  <h4>Maritalat</h4>
 			  
-			  <p>« Le Maritalat déclarait : “la recherche de paternité est interdite” (Code Napoléon : Art. 340). »</p>
+			  
+			  <p>« Sous le Maritalat, affranchissement de la femme voulait dire “<b>trahison</b>” de son sexe inévitable,
+et “singer les hommes” à son détriment. Même problème que pour les juifs : la solution des transfuges
+ne règle pas le problème communautaire (cf. Marx, Spinoza, etc.). Ou alors, c’est l’émancipation
+<b>suicidaire</b> : citoyenneté “accordée” aux juifs français en 1791. Même tragédie pour la civilisation
+communautaire forcée : le baptême du chef des Saxons WITIKIND par Charlemagne en 786 »</p>
+			  
+ <p>« Le Maritalat déclarait : “la recherche de paternité est interdite” (Code Napoléon : Art. 340). »</p>
 <p>__________________________</p>
 
-<p>Voir <a href="https://www.eglise-realiste.org/pdf/Manuel_Handbook_Programme-Platform.pdf" target="_blank"> "Manuel Réaliste Convenable du Comm-Anar"</a>, p.74 (34) - 26.01-2012 - Talib freddy - Editions de l'Evidence</p>
+<p>Voir <a href="https://www.eglise-realiste.org/pdf/Manuel_Handbook_Programme-Platform.pdf" target="_blank"> "Manuel Réaliste Convenable du Comm-Anar"</a>, p.72-74 (33-34) - 26.01-2012 - Talib freddy - Editions de l'Evidence</p>
 			  
 <p>__________________________</p>
 
