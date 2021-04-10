e0953d84885de8d7623c3521a0fd6434317aa257
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-10 20:10:11 +0200

    no message

--- navigation-boussole.php
+++ navigation-boussole.php
@@ -11,7 +11,7 @@
 
 	<h5 style="text-align:center;font-size: 30px; font-family: 'Georgia';" class="black-text"><i>‘‘Le pur est le pire’’</i>,<br><i>‘‘L'Histoire n'est pas le Temps’’</i>, </h5> 
 
-<p style="font-family: 'Georgia';color:black;" >‘‘Mots Bijoux de Dédé’’ est une Poly-Prose philosophico-politique engagée. <br>Le but du jeu est de naviguer dans la causerie fraternelle-amicale... à l'aide de boussoles indiquant : la Victoire de l'Histoire.</p>
+<p style="font-family: 'Georgia';color:black;" >‘‘Mots Bijoux’’ est une Poly-Prose philosophico-politique engagée dans la Transformation de la Masse en Peuple. <br>Le but du jeu est de naviguer, à l'aide de boussoles, dans la causerie fraternelle-amicale...  pour la Victoire de l'Histoire.</p>
 
 <p style="margin-top: 200px;">
 		
@@ -29,8 +29,8 @@
 
 
 	<p style="margin-top: 40px">
-	<p style="font-family: 'Georgia';color:black;" >
-	 Préhistoire, Histoire, Epoque Moderne et Contemporaine... Revisitez ces moments en jouant à <a href="regledujeu.php">Mots Bijoux</a>...</p> 
+	<p style="font-family: 'Georgia';color:black;font-size: 16px;" >
+	Puisque nous n’avons plus que <i><b>“nos Chaînes à perdre, et un Monde à gagner !</b></i>”… (Marx-Engels : Manifeste), jouons à <a href="regledujeu.php">Mots Bijoux</a>...</p> 
 		
 
 
@@ -39,12 +39,12 @@
 
 
 	<hr>
-	<h5 style="    font-size: 55px; font-family: 'FrenchScriptMT';" class="black-text">‘‘On ne sait ni ne pouvons savoir pourquoi il y a un Monde... 
-		mais il est évident qu'il existe... Immense et Eternel’’ </h5> 
+	<h5 style="    font-size: 27px; font-family: 'georgia';" class="black-text"><b><i>‘‘Espèce Travailleuse, “CONNAIS-TOI TOI-MÊME !”</i></b> </h5> 
 		<h6 style="    color: black;
-    font-size: 25px;
+    font-size: 18px;
     text-align: right;
-    font-family: 'Georgia';">‘‘Réel Muet’’ - Aîné de Dom Deschamps - 18.11.2020<h6>
+    font-family: 'Georgia';">Devise de notre Nouveau Socratisme. Socrate (468-399 †) fit sienne cette devise gravée au fronton du Temple de Delphes (au pied du
+Parnasse), où Apollon rendait ses oracles par la bouche de la Pythie. Voir<a href="https://www.eglise-realiste.org/pdf/Manuel-Handbook_Programme-Platform.pdf" target="_blank"> ‘‘Manuel Programme du Comm-Anar’’</a> - Talib Freddy - 24.02.2012<h6>
 
 
 
