cef53d3fd9a36b497c22925eff4057fbe5db48ac
ORDI-ONE\Gwuiyano <to.martin.sanchez@gmail.com>
2021-04-11 07:37:31 +0200

    no message

--- modal/patience.php
+++ modal/patience.php
@@ -3,6 +3,30 @@
 			<div class="modal-content">
 			  <h4>Patience</h4>
 			  
+			  
+			  <p>« C’est fini : le ciel sur leur tête, et la boue sous leurs pieds ! Assis, déguenillés contre la
+bouche de l’égout, toute cette famille de travailleurs !</p>
+<p>S’ils voulaient pourtant, eh ! s’ils voulaient, ces prolétaires !</p>
+<p>Ils sont trente mille riches qui détiennent sur une surface de trente lieues carrées (le
+territoire parisien) le sol de la patrie. Nous sommes un million quatre cent mille qu’ils
+exploitent et qu’ils ruinent, un million quatre cent mille !</p>
+<p>Ils y ont mis de la patience ! Mais aujourd’hui les pauvres savent ce qu’ils valent, et que
+le travail est tout, et qu’il n’est pas une fortune qui n’ait ses racines dans la boue féconde
+des faubourgs !</p>
+<p>Propriétaires de Paris, la République vous laisse un moment de réflexion encore… Mais
+si vous restiez obstinés et méprisants, qui sait quel danger vous menace ?…</p>
+<p>Ce serait une révolte sinistre et muette.</p>
+<p>Elle n’a pas encore de nom dans l’histoire. Elle en aura un :</p>
+<p style="text-align:center;"><b>LA GRÈVE DU LOYER.</b></p>
+	
+
+<p>__________________________</p>
+
+	 
+			 
+			  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Commune_gde.pdf" target="_blank"> "Vive la Commune"</a>, p.215: <i>"Le Cri du Peuple -  La grève du loyer (8 mars 1871)"</i> - Freddy Malot - 1971 - </p>
+
+			  <p>__________________________</p>
 			  <p>« La Masse Informe initiale n’est pas “vierge” mentalement, mais pénétrée des mentalités
 “préhistoriques” Spiritualiste et Matérialiste. Cette pensée vivante de la Masse est traquée
 sans relâche par la Caste dominante, qui y oppose finalement en vain son Paganisme Intégral,
@@ -32,7 +56,7 @@ dominante. »</p>
 			  <p>__________________________</p>
 
 			  <p>« L’apparition du Sauveur à la petite troupe des onze, et la victoire qu’il y remporta sur l’infidélité d’un disciple, est aujourd’hui l’objet spécial du culte de la sainte Église. Cette apparition, qui se lie à la précédente, est la septième ; par elle Jésus entre en possession complète de la foi de ses disciples. Sa dignité, sa patience, sa charité, dans cette scène, sont véritablement d’un Dieu. Là encore, nos pensées humaines sont renversées, à la vue de ce délai que Jésus accorde à l’incrédule, dont il semblerait devoir éclairer sans retard l’aveuglement malheureux, ou châtier l’insolence téméraire. Mais Jésus est la souveraine sagesse et la souveraine bonté ; dans sa sagesse, il ménage, par cette lente confrontation du fait de sa Résurrection, un nouvel argument en faveur de la réalité de ce fait ; dans sa bonté, il amène le coeur du disciple incrédule à rétracter de lui-même son doute par une protestation sublime de regret, d’humilité et d’amour. Nous ne décrirons point ici cette scène si admirablement retracée dans le récit de l’Évangile que la sainte Église va tout à l’heure mettre sous nos yeux. Nous nous attacherons, pour la doctrine de ce jour, à faire comprendre au lecteur la leçon pieuse que Jésus donne aujourd’hui à tous, en la personne de saint Thomas. C’est le grand enseignement du Dimanche de l’Octave de Pâques ; il importe de ne le pas négliger ; car il nous révèle, plus que tout autre, le véritable sens du christianisme ; il nous éclaire sur la cause de nos impuissances, sur le remède de nos langueurs.</p>
-<p>Jésus dit à Thomas : “Tu as cru, parce que tu as vu ; heureux ceux qui n’ont pas vu et qui néanmoins ont cru !” Paroles remplies d’une divine autorité, conseil salutaire donné non seulement à Thomas, mais à tous les hommes qui veulent entrer en rapport avec Dieu et sauver leurs âmes. Que voulait donc Jésus de son disciple ? Ne venait-il pas de l’entendre confesser la foi dont il était désormais pénétré ? Thomas, d’ailleurs, était-il si coupable d’avoir désiré l’expérience personnelle, avant de donner son adhésion au plus étonnant des prodiges ? Était-il tenu de s’en rapporter à Pierre et aux autres, au point d’avoir à craindre de manquer à son Maître, en ne déférant pas à leur témoignage ? Ne faisait-il pas preuve de prudence en suspendant sa conviction, jusqu’à ce que d’autres arguments lui eussent révélé à lui-même que le fait était tel que ses frères le lui racontaient ? Oui, Thomas était un homme sage, un homme prudent, qui ne se confiait pas outre mesure ; il était digne de servir de modèle à beaucoup de chrétiens qui jugent et raisonnent comme lui dans les choses de la foi. Cependant, combien est accablant dans sa douceur si pénétrante, le reproche de Jésus ! Il a daigné se prêter, avec une condescendance inexplicable, à l’insolente vérification que Thomas avait osé demander ; maintenant que le disciple tremble devant le divin ressuscité, et qu’il s’écrie dans l’émotion la plus sincère : “Oh ! vous êtes bien mon Seigneur et mon Dieu !” Jésus ne lui fait pas grâce de la leçon qu’il avait méritée. Il faut un châtiment à cette hardiesse, à cette incrédulité ; et ce châtiment consisterai s’entendre dire : “Tu as cru, Thomas, parce que tu as vu.” Mais Thomas était-il donc obligé de croire avant d’avoir vu ? Et qui peut en douter ? Non seulement Thomas, mais tous les Apôtres étaient tenus de croire à la résurrection de leur maître, avant même qu’il se fût montré à eux. N’avaient-ils pas vécu trois années dans sa compagnie ? Ne l’avaient-ils pas vu confirmer par les plus divins prodiges sa qualité de Messie et de Fils de Dieu ? Ne leur avait-il pas annoncé sa résurrection pour le troisième jour après sa mort ? et quant aux humiliations et aux douleurs de sa Passion, ne leur avait-il pas dit, peu de temps auparavant, sur la route de Jérusalem, qu’il allait être saisi par les Juifs qui le livreraient aux gentils ; qu’il serait flagellé, couvert de crachats et mis à mort ? (Luc, XVIII, 32, 33.)»</p>
+<p>Jésus dit à Thomas : “Tu as cru, parce que tu as vu ; heureux ceux qui n’ont pas vu et qui néanmoins ont cru !” Paroles remplies d’une divine autorité, conseil salutaire donné non seulement à Thomas, mais à tous les hommes qui veulent entrer en rapport avec Dieu et sauver leurs âmes. Que voulait donc Jésus de son disciple ? Ne venait-il pas de l’entendre confesser la foi dont il était désormais pénétré ? Thomas, d’ailleurs, était-il si coupable d’avoir désiré l’expérience personnelle, avant de donner son adhésion au plus étonnant des prodiges ? Était-il tenu de s’en rapporter à Pierre et aux autres, au point d’avoir à craindre de manquer à son Maître, en ne déférant pas à leur témoignage ? Ne faisait-il pas preuve de prudence en suspendant sa conviction, jusqu’à ce que d’autres arguments lui eussent révélé à lui-même que le fait était tel que ses frères le lui racontaient ? Oui, Thomas était un homme sage, un homme prudent, qui ne se confiait pas outre mesure ; il était digne de servir de modèle à beaucoup de chrétiens qui jugent et raisonnent comme lui dans les choses de la foi. Cependant, combien est accablant dans sa douceur si pénétrante, le reproche de Jésus ! Il a daigné se prêter, avec une condescendance inexplicable, à l’insolente vérification que Thomas avait osé demander ; maintenant que le disciple tremble devant le divin ressuscité, et qu’il s’écrie dans l’émotion la plus sincère : “Oh ! vous êtes bien mon Seigneur et mon Dieu !” Jésus ne lui fait pas grâce de la leçon qu’il avait méritée. Il faut un châtiment à cette hardiesse, à cette incrédulité ; et ce châtiment consisterai s’entendre dire : “Tu as cru, Thomas, parce que tu as vu.” Mais Thomas était-il donc obligé de croire avant d’avoir vu ? Et qui peut en douter ? Non seulement Thomas, mais tous les Apôtres étaient tenus de croire à la résurrection de leur maître, avant même qu’il se fût montré à eux. N’avaient-ils pas vécu trois années dans sa compagnie ? Ne l’avaient-ils pas vu confirmer par les plus divins prodiges sa qualité de Messie et de Fils de Dieu ? Ne leur avait-il pas annoncé sa résurrection pour le troisième jour après sa mort ? et quant aux humiliations et aux douleurs de sa Passion, ne leur avait-il pas dit, peu de temps auparavant, sur la route de Jérusalem, qu’il allait être saisi par les Juifs qui le livreraient aux gentils ; qu’il serait flagellé, couvert de crachats et mis à mort ? (Luc, XVIII, 32, 33.) »</p>
 
 <p>__________________________</p>
 
@@ -40,8 +64,19 @@ dominante. »</p>
 
 <p>__________________________</p>
 
+
+<p><i>« L’Église a des enfants parmi ses ennemis, et des ennemis parmi ses enfants.</i></p>
+<p>Qu’elle réponde ainsi à ses adversaires, quelle réponde plus abondamment et plus pertinemment encore, si cela est possible, la famille rachetée de Notre Seigneur Jésus-Christ, la cité de ce grand roi, étrangère ici-bas. Qu’elle se souvienne toutefois que ses ennemis cachent dans leurs rangs plusieurs de ses futurs concitoyens, de peur qu’elle ne regarde comme stérile à leur égard la patience avec laquelle elle doit les supporter comme ennemis jusqu’au jour où elle les recevra dans son sein comme confesseurs. Qu’elle se souvienne aussi que, pendant son pèlerinage en ce monde, plusieurs lui sont unis par la communion des sacrements, qui n’auront point de part avec elle dans la gloire éternelle des saints. Ouvertement ou dans l’ombre, ces hommes marqués du sceau divin ne craignent pas de murmurer contre Dieu avec ses ennemis, se mêlant tantôt à eux dans les théâtres, tantôt à nous dans les églises. Mais il ne faut nullement désespérer du retour de plusieurs d’entre eux, puisque parmi ceux-là même qui font profession d’être nos ennemis les plus irréconciliables, nous avons des amis qui sont prédestinés au salut, sans qu’eux-mêmes s’en doutent. Les deux cités sont en effet mêlées et confondues dans le Siècle jusqu’à ce que le jugement dernier les sépare. C’est de leur naissance, de leurs progrès, et de la fin qui les attend, que j’ai dessein de traiter, avec l’assistance divine, pour la gloire de la cité de Dieu, qui tirera de ce contraste un nouvel éclat. »</p>
+
+<p>__________________________</p> 
+			  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Mentalite_Religieuse_ed.pdf" target="_blank"> "La Mentalité Religieuse"</a>, p. 128 <i>Annexes: St Augutin, La Cité de Dieu</i> - Talib Freddy - mars 2004 - Eglise Réaliste Mondiale - Editions de l'Evidence - 2010</p>
+ 
+
+<p>__________________________</p>
+
 			  <p>« Insistons sur le fait que dans cette aventure de la Nouvelle Religion, <b>chaque confession reste elle-même autant qu’elle le désire</b>. Tout se passe dans une Émulation enthousiasmante, chaque confession Différente recherchant seulement la position d’avant-garde au service de LA religion UNIQUE dont toutes les confessions participent. Telle est l’originalité, la condition inouïe, que présente l’Orthodoxie Nouvelle : le service de l’Unique religion, celle totalement agréée par Dieu évidemment. (Le SEUL Musulman que j’ai connu, qui croyait VRAIMENT que toute l’humanité adopterait l’Islam, c’est Sayyed Qutb. Nous sommes dans sa voie, comprise au sens large ; et j’ose le voir se réjouir de notre projet.) »</p>
 
+			  
 			  <p>__________________________</p>
 
 			  <p>Voir <a href="https://www.eglise-realiste.org/pdf/Nouvelle_Religion_ed.pdf" target="_blank"> "La Nouvelle Religion"</a>, p. 18 - Talib Freddy - août 2007 - Editions de l'Evidence - 2010</p>
